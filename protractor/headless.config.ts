b6f3e790d168ec4a7b857c1eb15d2ddf59c4eeb3
Andrés Felipe Aguilar Rendón <afaguilarr96@gmail.com>
2018-06-14 14:47:01 -0500

    static code added (#10)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -11,9 +11,9 @@ export const config: Config = {
     chromeOptions: {
       args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
     }
-  }, 
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
   }
-}
+};
