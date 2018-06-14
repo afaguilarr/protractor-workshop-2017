8a90dc0a77bf629126808eba992b2849ee404a6e
Andrés Felipe Aguilar Rendón <afaguilarr96@gmail.com>
2018-06-14 16:33:25 -0500

    CSS selectors working (#11)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,16 +4,17 @@ import { reporter }   from './helpers/reporter';
 export const config: Config = {
   framework: 'jasmine',
   SELENIUM_PROMISE_MANAGER: false,
-  specs: ['../test/Google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   noGlobals: true,
+  getPageTimeout: 1000,
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--disable-gpu', '--window-size=800,600', '--no-sandbox']
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
     }
-  },
+  }, 
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
   }
-};
+}
