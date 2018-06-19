4a8e82583a082349cd1b9990df512e566ce73ee5
Andrés Felipe Aguilar Rendón <afaguilarr96@gmail.com>
2018-06-19 10:13:44 -0500

    Used the capabalities argument in the configuration files to disable infobars in chrome (#19)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -10,8 +10,9 @@ export const config: Config = {
   capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-      args: ['--headless', '--disable-gpu', '--window-size=800,600']
-    }
+      args: ['--headless', '--disable-gpu', 'disable-infobars=true --window-size=800,600']
+    },
+    prefs: { credentials_enable_service: false }
   },
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
